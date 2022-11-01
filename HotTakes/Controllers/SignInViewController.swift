5e4a1f70fa694da771f747fea5ad80c1f16f87cb
mikekulka <45544526+mikekulka@users.noreply.github.com>
2022-10-31 21:22:19 -0400

    To create sign in and sign up storyboards

---
+++ HotTakes/Controllers/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  HotTakes
+//
+//  Created by Michael Kulka on 10/31/22.
+//
+
+import Foundation
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ HotTakes/Controllers/SignUpViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewController.swift
+//  HotTakes
+//
+//  Created by Michael Kulka on 10/31/22.
+//
+
+import Foundation
